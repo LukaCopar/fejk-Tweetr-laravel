d3818765f12c88fc5222912e79a2b3facb49012d
Luka Čopar <luka.copar@gmail.com>
2018-11-06 11:27:54 +0100

    users wiew inserted

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,16 +4,17 @@
 <div class="jumbotron text-center">
         <h1>{{$data}}</h1>
         <p>users</p>
-        <hr>
+        
         @foreach ($users as $user)
+        <hr>
         <div class="well" style=" padding-bottom: 2px; float:left; max_width: 100%; height: 40px;">
-                        <h3><a href="/users/{{$user->id}}"> {{$user -> name}}</a></h3>
-                          <span style="max-width: 100%; max-height: 40px;">{{$user->created_at}}</span>
-                         
+                        <span style="font-size: 30px;"><a href="/users/{{$user->id}}"> {{$user -> name}}</a></span>
+                          <span style="max-width: 100%; max-height: 40px; white-space: normal;">joined: {{$user->created_at}}</span>    
+                          <hr/>
+                        </div> 
+
                         
-                </div> 
-                @break
       @endforeach
-   
+      
 
 @endsection
